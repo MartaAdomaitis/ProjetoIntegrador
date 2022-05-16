27106394c2290df12fe37062ce86810e33522759
Marta Adomaitis <martaadomaitis@gmail.com>
2022-05-16 00:46:58 -0300

    detalheProdutos +correção server commit

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const path = require('path');
 
-const indexRouter = require('./src/routes/index');
-const usersRouter = require('./src/routes/users');
+const indexRouter = require('./src/routes/index.js');
+
 
 const app = express();
 
-app.get("./", (req,res)=>{
-  return (indexRouter);
+app.get("/", (req,res)=>{
+  res.render ("index");
 });
+
+app.get("/destalhesproduto", (req,res)=>{
+  res.render ("detalhesProduto");
+});
+
 app.listen(3000, ()=>{
   console.log('server is running');
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, './views'));
+app.set('views', path.join(__dirname, './src/views'));
+app.use(express.static(__dirname + './projetoIntegrador/public'));
 app.set('view engine', 'ejs');
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('funcionou!');
+  res.render("../views/index.ejs");
 });
 
 module.exports = router;
