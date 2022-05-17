65c3b5ec34d679d796bb0004f997120aba7c3884
marta.adomaitis <martaadomaitis@gmail.com>
2022-05-17 19:56:58 -0300

    avanços header e footer

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.listen(3000, ()=>{
 // view engine setup
 app.set('views', path.join(__dirname, './src/views'));
 app.use( express.static( "public/img" ));
+app.use( express.static( "public/css" ));
 app.set('view engine', 'ejs');
 
 module.exports = app;
