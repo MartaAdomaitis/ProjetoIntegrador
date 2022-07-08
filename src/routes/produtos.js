22d286e77e9dac2c6cb07c878cfca0ed8042df47
Amanda Carvalho Belmont <carvalhoamandati@gmail.com>
2022-07-07 22:36:52 -0300

    Sequelize

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require("multer");
 
-// ************ Controller Require ************
+// ************ Controller Require ***********
 const productsController = require('../controllers/productsController');
 
 /*** GET ALL PRODUCTS ***/ 
