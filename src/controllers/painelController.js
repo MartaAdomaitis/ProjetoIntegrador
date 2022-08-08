5e933f9a89842a505b4c48de0d200a23360ee4f5
Amanda Carvalho Belmont <carvalhoamandati@gmail.com>
2022-08-08 11:02:57 -0300

    painelAtualizar

--- src/controllers/painelController.js
+++ src/controllers/painelController.js
@@ -1,10 +1,11 @@
 const db = require('../database/models/index');
+const users = require('../database/models/produto');
 
 const updatepainelController = {
     updatepainelUsuario: async(req, res) => {
         const { id_usuario, email, senha} = req.body;
         try {
-            const updatepainelController = await dbpainelController.update({ id_usuario, email, senha })
+            const getupdatepainelController = await db.users.findAll({ id_usuario, email, senha })
 
         } catch (error) {}
             console.log("error")  
