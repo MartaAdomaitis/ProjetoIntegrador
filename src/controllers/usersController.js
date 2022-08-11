73cff611cadf12922c468aa88b99f623b93d0eaa
Marta Adomaitis <martaadomaitis@gmail.com>
2022-08-11 20:31:31 -0300

    Ajuste home

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -24,7 +24,7 @@ const usersController = {
 	create: (req,res) => {
 	const nome = req.body.nome;
 	const email = req.body.email;
-	const senha = req.body.senha;
+	const senha = bcrypt.hashSync(req.body.senha,10);
 	const endereco = req.body.endereco;
 	const telefone = req.body.telefone;
 	const cpf = req.body.cpf;
@@ -47,13 +47,18 @@ console.log("Ops, houve um erro:" + err)
 login: (req,res) => {
 	const emailUsuario = req.body.emailLogin;
 	const senhaUsuario = req.body.senhaLogin;
-	const loginUser = Usuario.findOne({
-
-	where:{
-	email: emailUsuario,
-	senha: senhaUsuario,
-},
-	})
+	const loginUser = Usuario.findOne({	where:{
+	email: emailUsuario,}}, (err,Usuario)=>{
+		if (err) {
+			next(err);
+	} else {
+        if (bcrypt.compareSync(req.body.senhaLogin, Usuario.senha)) {
+          const token = jwt.sign({ id: Usuario.id }, req.app.get('secretKey'), { expiresIn: '1h' });
+          res.json({ status: "Sucesso", message: "Usuário encontrado!", data: { user: Usuario, token: token } });
+        } else {
+          res.json({ status: "error", message: "E-mail e senha inválidos", data: null });
+        }
+	}})
 	.then((loginUser)=>{
 		if (loginUser != null){
 		console.log("Login realizado com sucesso!");
